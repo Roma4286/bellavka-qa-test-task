c43713171b8f2f34fb2c7a450bddcf936806e9a7
Roma4286 <lionkrou8@gmail.com>
2026-03-15 16:59:09 +0300

    fix: improve modal closing logic

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -1,22 +1,30 @@
-import { test } from '@playwright/test';
+import { test, type Page } from '@playwright/test';
+
+async function closeModalIfVisible(page: Page) {
+  const modal = page.locator('div.v-modal.sale[role="dialog"]');
+  const closeButton = modal.locator('.v-modal__close');
+
+  await modal.waitFor({ state: 'visible' });
+
+  await closeButton.click();
+
+  await modal.waitFor({ state: 'hidden' });
+}
 
 test('Add product to cart', async ({ page }) => {
   await page.goto('https://ramonki.by/');
 
-  const modal = page.locator('[role="dialog"]');
-  const closeButton = page.locator('.v-modal__close');
-
-  if (await modal.isVisible()) {
-    await closeButton.click();
-    await modal.waitFor({ state: 'hidden' });
-  }
-
+  await closeModalIfVisible(page);
   await page.getByRole('button', { name: 'Каталог' }).click();
   const clothesLink = page.getByRole('link', { name: /^Одежда$/ });
-  await Promise.all([page.waitForNavigation(), clothesLink.click()]);
+
+  await clothesLink.click();
+  await page.waitForURL('**/catalog/cat-odezda-1782510');
 
   const dressesLink = page.locator('li.filter__category', {
     hasText: 'Платья и сарафаны',
   });
-  await Promise.all([page.waitForNavigation(), dressesLink.click()]);
+
+  await dressesLink.click();
+  await page.waitForURL('**/catalog/cat-platia-i-sarafany-38365');
 });
